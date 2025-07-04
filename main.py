33cc771e63d81696635c9f16a81de7ce088216cb
Lo0rd3 <joaofmp99@gmail.com>
2025-07-04 09:57:05 +0100

    added cheatsheets

--- main.py
+++ main.py
@@ -3,24 +3,28 @@ from resumo import generateSumary
 from quiz import interactiveQuiz
 from flashcards import interactFlashcards
 from flashcards import genFlashcards
+from cheatSheet import generateCheatSheet
 api_key = getApiKey()
 try:
     while True:
         print("\n==== Assistente de Estudos ====")
-        print("[1] Gerar resumo")
-        print("[2] Fazer quiz interativo")
-        print("[3] Gerar flashcards")
-        print("[4] Praticar flashcards")
+        print("[1] Gerar Resumos")
+        print("[2] Gerar CheatSheets")
+        print("[3] Fazer Quiz Interativo")
+        print("[4] Gerar Flashcards")
+        print("[5] Praticar Flashcards")
         print("[0] Sair")
         choice = input("Escolha uma opção: ").strip()
 
         if choice == "1":
             generateSumary()
         elif choice == "2":
-          interactiveQuiz()
+            generateCheatSheet()
         elif choice == "3":
-             genFlashcards()
+          interactiveQuiz()
         elif choice == "4":
+             genFlashcards()
+        elif choice == "5":
             interactFlashcards()
         elif choice == "0":
             print("Até logo!")
